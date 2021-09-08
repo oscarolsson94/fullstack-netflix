d97abac208754c5299f74ea860b723f0391361d0
oscarolsson94 <oscar.olsson94@hotmail.com>
2021-09-08 11:51:03 +0200

    adding more items to navbar and styling

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import { Notifications, Search } from "@material-ui/icons";
+import { ArrowDropDown, Notifications, Search } from "@material-ui/icons";
 import "./navbar.scss";
 
 const Navbar = () => {
@@ -24,6 +24,13 @@ const Navbar = () => {
             src="https://images.pexels.com/photos/6899260/pexels-photo-6899260.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
             alt=""
           />
+          <div className="profile">
+            <ArrowDropDown className="icon" />
+            <div className="options">
+              <span>Settings</span>
+              <span>Logout</span>
+            </div>
+          </div>
         </div>
       </div>
     </div>
