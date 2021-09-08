2410148c52adabbb2da420605a62e7e98b070a33
oscarolsson94 <oscar.olsson94@hotmail.com>
2021-09-08 16:39:59 +0200

    connecting to mongodb

--- api/index.js
+++ api/index.js
@@ -1,7 +1,25 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
+dotenv.config();
+
+const PORT = process.env.PORT;
 const app = express();
 
-app.listen(3001, () => {
-  console.log("Backend running on port 3001");
+mongoose
+  .connect(process.env.MONGODB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .then(() => console.log("DB Connection Successfull"))
+  .catch((err) => {
+    console.error(err);
+  });
+
+app.use(express.json());
+
+app.listen(PORT, () => {
+  console.log(`Backend running on port ${PORT}`);
 });
