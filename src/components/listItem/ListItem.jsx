19f36cbcdd0c848dfc8f30da915124a93d665eee
oscarolsson94 <oscar.olsson94@hotmail.com>
2021-09-08 13:41:52 +0200

    styling listItem

--- src/components/listItem/ListItem.jsx
+++ src/components/listItem/ListItem.jsx
@@ -1,7 +1,14 @@
 import "./listItem.scss";
 
 const ListItem = () => {
-  return <div className="listItem">item</div>;
+  return (
+    <div className="listItem">
+      <img
+        src="https://occ-0-1723-92.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABU7D36jL6KiLG1xI8Xg_cZK-hYQj1L8yRxbQuB0rcLCnAk8AhEK5EM83QI71bRHUm0qOYxonD88gaThgDaPu7NuUfRg.jpg?r=4ee"
+        alt=""
+      />
+    </div>
+  );
 };
 
 export default ListItem;
