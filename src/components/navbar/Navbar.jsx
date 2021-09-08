59c13b3c59a2459e2002cdeb8b400004cff094f3
oscarolsson94 <oscar.olsson94@hotmail.com>
2021-09-08 12:17:04 +0200

    bugfix and adding components to Featured

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = () => {
   const [isScrolled, setIsScrolled] = useState(false);
 
   window.onscroll = () => {
-    setIsScrolled(window.scrollX === 0 ? false : true);
+    setIsScrolled(window.scrollY === 0 ? false : true);
     return () => (window.onscroll = null);
   };
   return (
