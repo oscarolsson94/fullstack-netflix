7fb189b6df4d8b1baa4396afc55f6ab4efa03698
oscarolsson94 <oscar.olsson94@hotmail.com>
2021-09-08 13:11:18 +0200

    handleClick function for slider functionality

--- src/components/list/List.jsx
+++ src/components/list/List.jsx
@@ -11,6 +11,19 @@ const List = () => {
   const [slideNumber, setSlideNumber] = useState(0);
 
   const listRef = useRef();
+
+  const handleClick = (direction) => {
+    setIsMoved(true);
+    let distance = listRef.current.getBoundingClientRect().x - 50;
+    if (direction === "left" && slideNumber > 0) {
+      setSlideNumber(slideNumber - 1);
+      listRef.current.style.transform = `translateX(${230 + distance}px)`;
+    }
+    if (direction === "right" && slideNumber < 5) {
+      setSlideNumber(slideNumber + 1);
+      listRef.current.style.transform = `translateX(${-230 + distance}px)`;
+    }
+  };
   return (
     <div className="list">
       <span className="listTitle">Continue to watch</span>
