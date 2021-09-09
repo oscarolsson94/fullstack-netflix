56dfd66eb99699babe57df01223f03b845a5fcce
oscarolsson94 <oscar.olsson94@hotmail.com>
2021-09-09 11:02:26 +0200

    implement react router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,32 @@
 import Home from "./pages/home/Home";
+import Watch from "./pages/watch/Watch";
 import "./app.scss";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
 
 const App = () => {
-  return <Home />;
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/movies">
+          <Home type="movies" />
+        </Route>
+        <Route path="/series">
+          <Home type="series" />
+        </Route>
+        <Route path="/watch">
+          <Watch />
+        </Route>
+      </Switch>
+    </Router>
+  );
 };
 
 export default App;
