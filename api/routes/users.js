6a937651fbd03a2d2ed2f9b590c8aadafde9c38e
oscarolsson94 <oscar.olsson94@hotmail.com>
2021-09-09 09:22:29 +0200

    get one user route

--- api/routes/users.js
+++ api/routes/users.js
@@ -47,6 +47,15 @@ router.delete("/:id", verify, async (req, res) => {
 });
 
 //GET ONE
+router.get("/find/:id", async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    const { password, ...info } = user._doc;
+    res.status(200).json(info);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 //GET ALL
 
