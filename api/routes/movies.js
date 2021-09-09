7a201167a567ae0ff44facee4281316c0d8bfa37
oscarolsson94 <oscar.olsson94@hotmail.com>
2021-09-09 09:55:44 +0200

    create new movie route

--- api/routes/movies.js
+++ api/routes/movies.js
@@ -3,3 +3,19 @@ import Movie from "../models/Movie.js";
 import verify from "../verifyToken.js";
 
 const router = express.Router();
+
+//CREATE
+
+router.post("/", verify, async (req, res) => {
+  if (req.user.isAdmin) {
+    const newMovie = new Movie(req.body);
+    try {
+      const savedMovie = await newMovie.save();
+      res.status(201).json(savedMovie);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  } else {
+    res.status(403).json("You are not allowed to create a new movie!");
+  }
+});
