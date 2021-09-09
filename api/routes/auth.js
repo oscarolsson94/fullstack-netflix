3fb0aec0def9c7ba97a9edd4a6484811f182a7d3
oscarolsson94 <oscar.olsson94@hotmail.com>
2021-09-09 08:41:05 +0200

    adding async and try-catch

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -4,14 +4,18 @@ import User from "../models/User.js";
 const router = express.Router();
 
 //REGISTER
-router.post("/register", (req, res) => {
+router.post("/register", async (req, res) => {
   const newUser = new User({
     username: req.body.username,
     email: req.body.email,
     password: req.body.password,
   });
-
-  const user = newUser.save();
+  try {
+    const user = await newUser.save();
+    res.status(201).json(user);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 export default router;
