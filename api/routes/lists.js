d05addf0e5e880f05f3ac82ba770ea7f30bb8e5e
oscarolsson94 <oscar.olsson94@hotmail.com>
2021-09-09 10:34:33 +0200

    delete list by id

--- api/routes/lists.js
+++ api/routes/lists.js
@@ -18,3 +18,17 @@ router.post("/", verify, async (req, res) => {
     res.status(403).json("You are not allowed!");
   }
 });
+
+//DELETE
+router.delete("/:id", verify, async (req, res) => {
+  if (req.user.isAdmin) {
+    try {
+      await List.findByIdAndDelete(req.params.id);
+      res.status(201).json("The list has been delete...");
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  } else {
+    res.status(403).json("You are not allowed!");
+  }
+});
