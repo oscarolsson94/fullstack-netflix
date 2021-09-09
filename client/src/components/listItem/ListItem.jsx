7fae28a355114c674adf4c470695f3db3143232b
oscarolsson94 <oscar.olsson94@hotmail.com>
2021-09-09 11:39:53 +0200

    mapping over data from DB

--- client/src/components/listItem/ListItem.jsx
+++ client/src/components/listItem/ListItem.jsx
@@ -7,10 +7,9 @@ import {
 } from "@material-ui/icons";
 import { useState } from "react";
 
-export default function ListItem({ index }) {
+export default function ListItem({ index, item }) {
   const [isHovered, setIsHovered] = useState(false);
-  const trailer =
-    "https://player.vimeo.com/external/371433846.sd.mp4?s=236da2f3c0fd273d2c6d9a064f3ae35579b2bbdf&profile_id=139&oauth2_token_id=57447761";
+
   return (
     <div
       className="listItem"
@@ -18,13 +17,10 @@ export default function ListItem({ index }) {
       onMouseEnter={() => setIsHovered(true)}
       onMouseLeave={() => setIsHovered(false)}
     >
-      <img
-        src="https://occ-0-1723-92.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABU7D36jL6KiLG1xI8Xg_cZK-hYQj1L8yRxbQuB0rcLCnAk8AhEK5EM83QI71bRHUm0qOYxonD88gaThgDaPu7NuUfRg.jpg?r=4ee"
-        alt=""
-      />
+      <img src={item.img} alt="" />
       {isHovered && (
         <>
-          <video src={trailer} autoPlay={true} loop />
+          <video src={item.trailer} autoPlay={true} loop />
           <div className="itemInfo">
             <div className="icons">
               <PlayArrow className="icon" />
@@ -33,15 +29,12 @@ export default function ListItem({ index }) {
               <ThumbDownOutlined className="icon" />
             </div>
             <div className="itemInfoTop">
-              <span>1 hour 14 mins</span>
-              <span className="limit">+16</span>
-              <span>1999</span>
+              <span>{item.duration}</span>
+              <span className="limit">+{item.limit}</span>
+              <span>{item.year}</span>
             </div>
-            <div className="desc">
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-              Praesentium hic rem eveniet error possimus, neque ex doloribus.
-            </div>
-            <div className="genre">Action</div>
+            <div className="desc">{item.desc}</div>
+            <div className="genre">{item.genre}</div>
           </div>
         </>
       )}
--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -35,10 +35,9 @@ const Home = ({ type }) => {
     <div className="home">
       <Navbar />
       <Featured type={type} />
-      <List />
-      <List />
-      <List />
-      <List />
+      {lists.map((list) => (
+        <List list={list} />
+      ))}
     </div>
   );
 };
