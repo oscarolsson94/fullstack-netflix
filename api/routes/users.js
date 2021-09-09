f14e7701f050399bc6e46f87fc56e959c36aad9a
oscarolsson94 <oscar.olsson94@hotmail.com>
2021-09-09 09:38:25 +0200

    get all users as admin

--- api/routes/users.js
+++ api/routes/users.js
@@ -58,6 +58,21 @@ router.get("/find/:id", async (req, res) => {
 });
 
 //GET ALL
+router.get("/", verify, async (req, res) => {
+  const query = req.query.new; //if there is a query "?new=true", fetch 5 latest users
+  if (req.user.isAdmin) {
+    try {
+      const users = query
+        ? await User.find().sort({ _id: -1 }).limit(5)
+        : await User.find();
+      res.status(200).json(users);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  } else {
+    res.status(403).json("You are not allowed to see all users!");
+  }
+});
 
 //GET USER STATS
 
