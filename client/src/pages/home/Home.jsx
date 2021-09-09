659cd13bdbc38b0635f5058d0c088d43118ede1e
oscarolsson94 <oscar.olsson94@hotmail.com>
2021-09-09 11:31:57 +0200

    fix proxy bug and setup axios call

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -3,18 +3,34 @@ import Navbar from "../../components/navbar/Navbar";
 import List from "../../components/list/List";
 import "./home.scss";
 import { useEffect, useState } from "react";
+import axios from "axios";
 
 const Home = ({ type }) => {
   const [lists, setLists] = useState([]);
+  const [genre, setGenre] = useState(null);
 
   useEffect(() => {
     const getRandomLists = async () => {
       try {
-      } catch (error) {
-        console.log(error);
+        const res = await axios.get(
+          `lists${type ? "?type=" + type : ""}${
+            genre ? "&genre=" + genre : ""
+          }`,
+          {
+            headers: {
+              token:
+                "Bearer " +
+                JSON.parse(localStorage.getItem("user")).accessToken,
+            },
+          }
+        );
+        setLists(res.data);
+      } catch (err) {
+        console.log(err);
       }
     };
-  });
+    getRandomLists();
+  }, [type, genre]);
   return (
     <div className="home">
       <Navbar />
