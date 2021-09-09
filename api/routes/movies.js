ccf7171cf4c015440c9545684309453121db45ef
oscarolsson94 <oscar.olsson94@hotmail.com>
2021-09-09 10:23:40 +0200

    return movie based on query

--- api/routes/movies.js
+++ api/routes/movies.js
@@ -65,6 +65,29 @@ router.get("/find/:id", verify, async (req, res) => {
   }
 });
 
+//GET RANDOM
+router.get("/random", verify, async (req, res) => {
+  /* /random?type=series will return random series, else return random movie */
+  const type = req.query.type;
+  let movie;
+  try {
+    if (type === "series") {
+      movie = await Movie.aggregate([
+        { $match: { isSeries: true } },
+        { $sample: { size: 1 } },
+      ]);
+    } else {
+      movie = await Movie.aggregate([
+        { $match: { isSeries: false } },
+        { $sample: { size: 1 } },
+      ]);
+    }
+    res.status(200).json(movie);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 //GET ALL
 router.get("/", verify, async (req, res) => {
   if (req.user.isAdmin) {
