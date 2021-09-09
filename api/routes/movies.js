34bbc7054a377bedd951f82c4a0b5febfc862adb
oscarolsson94 <oscar.olsson94@hotmail.com>
2021-09-09 10:00:09 +0200

    update movie

--- api/routes/movies.js
+++ api/routes/movies.js
@@ -5,7 +5,6 @@ import verify from "../verifyToken.js";
 const router = express.Router();
 
 //CREATE
-
 router.post("/", verify, async (req, res) => {
   if (req.user.isAdmin) {
     const newMovie = new Movie(req.body);
@@ -17,6 +16,26 @@ router.post("/", verify, async (req, res) => {
     }
   } else {
     res.status(403).json("You are not allowed to create a new movie!");
+  }
+});
+
+//UPDATE
+router.put("/:id", verify, async (req, res) => {
+  if (req.user.isAdmin) {
+    try {
+      const updatedMovie = await Movie.findByIdAndUpdate(
+        req.params.id,
+        {
+          $set: req.body,
+        },
+        { new: true }
+      );
+      res.status(200).json(updatedMovie);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  } else {
+    res.status(403).json("You are not allowed!");
   }
 });
 
