5b550eed98c8010df048ca1a985c43eaf0f5002a
oscarolsson94 <oscar.olsson94@hotmail.com>
2021-09-09 11:18:51 +0200

    getting axiois & set up useState and useEffect

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -2,8 +2,19 @@ import Featured from "../../components/featured/Featured";
 import Navbar from "../../components/navbar/Navbar";
 import List from "../../components/list/List";
 import "./home.scss";
+import { useEffect, useState } from "react";
 
 const Home = ({ type }) => {
+  const [lists, setLists] = useState([]);
+
+  useEffect(() => {
+    const getRandomLists = async () => {
+      try {
+      } catch (error) {
+        console.log(error);
+      }
+    };
+  });
   return (
     <div className="home">
       <Navbar />
