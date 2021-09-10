9b541366c37f62a7ea6397d0fa311642ffbe3af2
oscarolsson94 <oscar.olsson94@hotmail.com>
2021-09-10 16:05:53 +0200

    added username to register

--- client/src/pages/register/Register.jsx
+++ client/src/pages/register/Register.jsx
@@ -1,18 +1,31 @@
+import axios from "axios";
 import { useRef, useState } from "react";
+import { useHistory } from "react-router";
 import "./register.scss";
 
 const Register = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [username, setUsername] = useState("");
+  const history = useHistory();
 
   const emailRef = useRef();
   const passwordRef = useRef();
+  const usernameRef = useRef();
 
   const handleStart = () => {
     setEmail(emailRef.current.value);
   };
-  const handleFinish = () => {
+  const handleFinish = async (e) => {
+    e.preventDefault();
     setPassword(passwordRef.current.value);
+    setUsername(usernameRef.current.value);
+    try {
+      await axios.post("auth/register", { email, username, password });
+      history.push("/login");
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   return (
@@ -42,6 +55,7 @@ const Register = () => {
           </div>
         ) : (
           <form className="input">
+            <input type="username" placeholder="username" ref={usernameRef} />
             <input type="password" placeholder="password" ref={passwordRef} />
             <button className="registerButton" onClick={handleFinish}>
               Start Membership
