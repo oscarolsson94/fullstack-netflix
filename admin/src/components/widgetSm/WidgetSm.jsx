c6ef4c3fc793915a3e07fd03c9c5e7591d91aa1d
oscarolsson94 <oscar.olsson94@hotmail.com>
2021-09-10 08:56:19 +0200

    fetching new users

--- admin/src/components/widgetSm/WidgetSm.jsx
+++ admin/src/components/widgetSm/WidgetSm.jsx
@@ -1,7 +1,24 @@
 import "./widgetSm.css";
 import { Visibility } from "@material-ui/icons";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 export default function WidgetSm() {
+  const [newUsers, setNewUsers] = useState([]);
+
+  useEffect(() => {
+    const getNewUsers = async () => {
+      try {
+        const res = await axios.get("/users?new=true", {
+          headers: { token: "Bearer ......." },
+        });
+        setNewUsers(res.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getNewUsers();
+  }, []);
   return (
     <div className="widgetSm">
       <span className="widgetSmTitle">New Join Members</span>
