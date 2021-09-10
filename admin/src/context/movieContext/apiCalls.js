8f264ea82845fca5ae2e67010c0bf87ee8591d64
oscarolsson94 <oscar.olsson94@hotmail.com>
2021-09-10 11:18:00 +0200

    create and delete movie axios calls

--- admin/src/context/movieContext/apiCalls.js
+++ admin/src/context/movieContext/apiCalls.js
@@ -1,5 +1,11 @@
 import axios from "axios";
 import {
+  createMovieFailure,
+  createMovieStart,
+  createMovieSuccess,
+  deleteMovieFailure,
+  deleteMovieStart,
+  deleteMovieSuccess,
   getMoviesFailure,
   getMoviesStart,
   getMoviesSuccess,
@@ -16,5 +22,35 @@ export const getMovies = async (dispatch) => {
     dispatch(getMoviesSuccess(res.data));
   } catch (err) {
     dispatch(getMoviesFailure());
+  }
+};
+
+//create
+export const createMovie = async (movie, dispatch) => {
+  dispatch(createMovieStart());
+  try {
+    const res = await axios.post("/movies", movie, {
+      headers: {
+        token: "Bearer " + JSON.parse(localStorage.getItem("user")).accessToken,
+      },
+    });
+    dispatch(createMovieSuccess(res.data));
+  } catch (err) {
+    dispatch(createMovieFailure());
+  }
+};
+
+//delete
+export const deleteMovie = async (id, dispatch) => {
+  dispatch(deleteMovieStart());
+  try {
+    await axios.delete("/movies/" + id, {
+      headers: {
+        token: "Bearer " + JSON.parse(localStorage.getItem("user")).accessToken,
+      },
+    });
+    dispatch(deleteMovieSuccess(id));
+  } catch (err) {
+    dispatch(deleteMovieFailure());
   }
 };
