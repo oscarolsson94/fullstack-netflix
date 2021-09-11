1aacdce43d0ee31b31aada5dacaec11b21b60186
oscarolsson94 <oscar.olsson94@hotmail.com>
2021-09-11 22:48:14 +0200

    working on new user

--- admin/src/pages/newUser/NewUser.jsx
+++ admin/src/pages/newUser/NewUser.jsx
@@ -1,53 +1,48 @@
 import "./newUser.css";
+import { useContext, useState } from "react";
+import { createUser } from "../../context/userContext/apiCalls";
+import { UserContext } from "../../context/userContext/UserContext";
 
 export default function NewUser() {
+  const [user, setUser] = useState(null);
+
+  const { dispatch } = useContext(UserContext);
+
+  const handleChange = (e) => {
+    const value = e.target.value;
+    setUser({ ...user, [e.target.name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    createUser(user, dispatch);
+  };
+
   return (
-    <div className="newUser">
-      <h1 className="newUserTitle">New User</h1>
-      <form className="newUserForm">
-        <div className="newUserItem">
-          <label>Username</label>
-          <input type="text" placeholder="john" />
-        </div>
-        <div className="newUserItem">
-          <label>Full Name</label>
-          <input type="text" placeholder="John Smith" />
-        </div>
-        <div className="newUserItem">
+    <div className="newProduct">
+      <h1 className="addProductTitle">New User</h1>
+      <form className="addProductForm">
+        <div className="addProductItem">
           <label>Email</label>
-          <input type="email" placeholder="john@gmail.com" />
+          <input
+            type="text"
+            placeholder="Email"
+            name="email"
+            onChange={handleChange}
+          />
         </div>
-        <div className="newUserItem">
+        <div className="addProductItem">
           <label>Password</label>
-          <input type="password" placeholder="password" />
+          <input
+            type="password"
+            placeholder="Password"
+            name="password"
+            onChange={handleChange}
+          />
         </div>
-        <div className="newUserItem">
-          <label>Phone</label>
-          <input type="text" placeholder="+1 123 456 78" />
-        </div>
-        <div className="newUserItem">
-          <label>Address</label>
-          <input type="text" placeholder="New York | USA" />
-        </div>
-        <div className="newUserItem">
-          <label>Gender</label>
-          <div className="newUserGender">
-            <input type="radio" name="gender" id="male" value="male" />
-            <label for="male">Male</label>
-            <input type="radio" name="gender" id="female" value="female" />
-            <label for="female">Female</label>
-            <input type="radio" name="gender" id="other" value="other" />
-            <label for="other">Other</label>
-          </div>
-        </div>
-        <div className="newUserItem">
-          <label>Active</label>
-          <select className="newUserSelect" name="active" id="active">
-            <option value="yes">Yes</option>
-            <option value="no">No</option>
-          </select>
-        </div>
-        <button className="newUserButton">Create</button>
+        <button className="addProductButton" onClick={handleSubmit}>
+          Create
+        </button>
       </form>
     </div>
   );
